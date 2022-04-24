ca2d66f764565cc934d23f1d4400891dd7ae3135
EfrenOR <efren.or@outlook.com>
2022-04-23 19:01:28 -0500

    I used fragments and a template to optimize the web, and now I can display each movie of the object automatically and depending on their categories

--- js/show.js
+++ js/show.js
@@ -1,59 +1,167 @@
 
-//Parent Element
-const $sectionMovies = document.querySelector(".section-movies-horror")
 
 //Object with movies
 const moviesList = [
   {
     title : "Don't Breathe 2",
-    img : "Don't Breathe 2.jpg"
+    category : "Horror",
+    img : "Don't Breathe 2.jpg",
+    site : "https://www.themoviedb.org/movie/482373-don-t-breathe-2?language=es-es",
+    top : false
   },
   {
     title: "Us",
+    category : "Horror",
     img: "US.jpg",
+    site : "https://www.themoviedb.org/movie/458723-us?language=es-es",
+    top : true
   },
   {
     title: "A Quite Place Part II",
-    img : "QuitePlace.jpg"
+    category : "Horror",
+    img : "QuitePlace.jpg",
+    site : "https://www.themoviedb.org/movie/520763-a-quiet-place-2?language=es-es",
+    top : true
   },
   {
     title: "Midsommar",
-    img : "Midsommar.jpg"
+    category : "Horror",
+    img : "Midsommar.jpg",
+    site : "https://www.themoviedb.org/movie/530385-midsommar?language=es-es",
+    top : false
+  },
+  {
+    title: "Toy Story 4",
+    category : "Fantasy",
+    img: "ToyStory4.jpg",
+    site : "https://www.themoviedb.org/movie/301528-toy-story-4?language=es-es",
+    top : false
+  },
+  {
+    title: "Pirates Caribbean",
+    category : "Fantasy",
+    img : "PiratesCaribbean.jpg",
+    site : "https://www.themoviedb.org/movie/166426-pirates-of-the-caribbean-dead-men-tell-no-tales?language=es-es",
+    top : false
+  },
+  {
+    title: "Alice in Wonderland",
+    category : "Fantasy",
+    img : "AliceWonderland.jpg",
+    site : "https://www.themoviedb.org/movie/12155-alice-in-wonderland?language=es-es",
+    top : false
+  },
+  {
+    title: "Abominable",
+    category : "Fantasy",
+    img : "Abominable.jpg",
+    site : "https://www.themoviedb.org/movie/431580-abominable?language=es-es",
+    top : false
+  },
+  {
+    title:"Moonfall",
+    category : "Action",
+    img : "Moonfall.jpg",
+    site : "https://www.themoviedb.org/movie/406759-moonfall?language=es-es",
+    top : false
+  },
+  {
+    title : "Batman",
+    category : "Action",
+    img : "Batman.jpg",
+    site : "https://www.themoviedb.org/movie/414906-the-batman?language=es-es",
+    top : true
+  },
+  {
+    title : "BlackLight",
+    category : "Action",
+    img : "BlackLight.jpg",
+    site : "https://www.themoviedb.org/movie/823625-blacklight?language=es-es",
+    top : true
   }
 ]
 
-//Set the title of the section "HORROR"
-$sectionMovies.firstElementChild.querySelector("span").textContent = "HORROR";
 
-//Parent Element of each element <article></article>
-const $sectionMoviesFlex = $sectionMovies.querySelector(".flex")
+/*With for and forEach() I created each cardsMovies of my object moviesList*/
+//Edit: I used fragments and template element to optimize the website
 
+//get the template
+$templateMovies = document.getElementById("cardsMovies").content
+//Create the fragments
+$fragmentMovies = document.createDocumentFragment();
+$fragmentMoviesTop = document.createDocumentFragment();
 
-//Create the cards for the movies/////
+let claseCategory;/*To store the category of each movie and use it to call
+  the section element with its class and append the Movies there
+*/
+let claseTop = false;//To know when a movie is top
 
-/*With forEach() I created each cardsMovies of my object moviesList*/
-moviesList.forEach((movie)=>{
+/*This is to store each category of the movies and I used Set()
+because doesn't accept replays*/
+let categories = new Set();
 
-  //Create Elements
-  const $cardsMovies = document.createElement("article"),
-        $figure = document.createElement("figure"),
-        $img = document.createElement("img"),
-        $titleMovie = document.createElement("h3");
-        $cardsMovies.classList.add("box-movies-horror") //Set a class
+moviesList.forEach((movie) => {//I stored each category in the categories variable of type set()
+  categories.add(movie.category)
+});
 
-  $img.setAttribute("src", `img/${movie.img}`)
-  $figure.appendChild($img)
-  $cardsMovies.appendChild($figure)
+/*I converted the categories variable above to Array, i did that because
+  I needed to access the values with their specific position
+*/
+let categoriesArray = Array.from(categories)
 
-  $titleMovie.textContent = movie.title;
-  $cardsMovies.appendChild($titleMovie)
+//Create each movie which is an <article> element in the HTML
+for (let i = 0; i <categoriesArray.length; i++) {
+  let position = i;
+  moviesList.forEach((movie)=>{
 
-  //Append each $cardsMovies to the Parent Element
-  $sectionMoviesFlex.appendChild($cardsMovies)
+    //This conditional is for put the "Top Movies"
+    if(movie.category == categoriesArray[position] && movie.top == true){
+      claseTop = true;//This is my flag variable
 
-})
+      //I set each Attribute of my elements inside $templateMovies
+      $templateMovies.querySelector("a").setAttribute("href", movie.site)
+      $templateMovies.querySelector("img").setAttribute("src", `img/${movie.img}`)
+      $templateMovies.querySelector("img").setAttribute("alt", movie.title)
+      $templateMovies.querySelector("h3").textContent = movie.title;
 
+      //I cloned the template to be able using it in other occasions
+      let $cloneTemplate = document.importNode($templateMovies, true)
 
+      //And I add the $cloneTemplate to the created $fragmentMoviesTop
+      $fragmentMoviesTop.appendChild($cloneTemplate)
+    }
 
+    //This is to put all movies depending on their category
+    if(movie.category == categoriesArray[position]){
+      claseCategory = movie.category//This stores the class depending on the category
+      $templateMovies.querySelector("a").setAttribute("href", movie.site)
+      $templateMovies.querySelector("img").setAttribute("src", `img/${movie.img}`)
+      $templateMovies.querySelector("img").setAttribute("alt", movie.title)
+      $templateMovies.querySelector("h3").textContent = movie.title;
+      let $cloneTemplate = document.importNode($templateMovies, true)
 
+      $fragmentMovies.appendChild($cloneTemplate)
+    }
+  })
+
+  /*I get the section depending on the category for this I need to use the created
+  claseCategory variable above
+  */
+  $sectionMovies = document.getElementsByClassName(`section-movies-${claseCategory}`)[0]
+
+  /*This conditional is to detect when a movies is "TOP" I need to use the flag
+    variable -> claseTop*/
+  if(claseTop == true){
+    $sectionMoviesTop = document.getElementsByClassName(`section-movies-Top`)[0]
+
+    //I add the $fragmentMoviesTop inside $sectionMoviesTop
+    $sectionMoviesTop.querySelector(".flex").append($fragmentMoviesTop)
+  }
+
+  //I add the $fragmentMovies inside $sectionMovies depending on the category
+  $sectionMovies.querySelector(".flex").append($fragmentMovies)
+
+}
+
+// Jair Efren Ortega Reyes
 //
